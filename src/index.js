0c77158663e38d6ba70b570c6b4f83f2146606a6
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-05-25 17:37:15 +0300

    added eventListener

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import PokemonsApi from "./js/pokemons-api";
 
+const mainButton = document.querySelector("#main-button")
+
+mainButton.addEventListener('click', getPokemons)
+
 const pokemonsApi = new PokemonsApi();
 
-pokemonsApi.getAllPokemonsData()
-.then(pokemonsData => {
-    console.log(pokemonsData)
-})
-.catch(error => {
-    console.log(error.message)
-})
+function getPokemons () {
+    pokemonsApi.getAllPokemonsData()
+        .then(pokemonsData => {
+            console.log(pokemonsData)
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+}
+
+
